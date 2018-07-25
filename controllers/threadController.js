05cfbddbc4eae2e22cf2202f52ba55320c8c0116
Benoît Gelineau <bgelineau.dev@gmail.com>
2018-07-25 21:20:53 +0200

    Add POST & GET methods

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -1,17 +1,46 @@
-const mongoUtil = require('../db');
-const db = mongoUtil.getDB();
+const getDb = require('../db').getDb;
+// const ObjectId = require('mongodb').ObjectId;
 
 exports.get_thread = (req, res, next) => {
+  const board = req.params.board;
+  const boardDb = getDb().collection(board);
+  
+  boardDb
+    .find({})
+    .limit(10)
+    .sort({ bumped_on: -1 })
+    .toArray()
+    .then(results => {
+      results.forEach(thread => {
+        // Take the last 3 items of the replies array (check with GET replies to change it or not)
+        thread.replies = thread.replies.slice(-3).reverse();
+      });
 
+      res.send(results);
+    })
+    .catch(error => console.log('Could not get this thread: ' + error));
 };
 
 exports.create_thread = (req, res, next) => {
-  // const board = req.body.board;
+  const board = req.params.board;
   const text = req.body.text;
   const pw = req.body.delete_password;
+  const boardDb = getDb().collection(board);
 
-  db.insertOne({ text: text });
-
+  boardDb.insertOne(
+    {
+      text: text,
+      created_on: new Date(),
+      bumped_on: new Date(),
+      reported: false,
+      delete_password: pw,
+      replies: []
+    }
+  )
+  .then(result => {
+    res.redirect(`/b/${board}`);
+  })
+  .catch(error => console.log('Could not create this thread: ' + error));
 
 };
 
